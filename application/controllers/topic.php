830d9484a717986c251ac67a1f4927650c56ca07
cyloops <cyloops@gmail.com>
2015-01-12 21:12:16 +0900

    Dashboard Version 0.0.1

---
+++ application/controllers/topic.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Topic extends CI_Controller {
+	function index(){
+		$this->load->view('head');
+		$this->load->view('topic');
+		$this->load->view('footer');
+	}
+
+	function get($id){
+		$this->load->view('head');
+		$this->load->view('get',array('id'=>$id));
+		$this->load->view('footer');
+	}
+}
