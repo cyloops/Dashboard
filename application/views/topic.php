6a3018f7b8ba1a3fa46748af908c936d16eb3a4b
cyloops <cyloops@gmail.com>
2015-01-13 17:49:25 +0900

    Fluid Nesting Test

--- application/views/topic.php
+++ application/views/topic.php
@@ -1,5 +1,15 @@
-
 <div class="row-fluid">
-  <div class="span4">...</div>
-  <div class="span8">...</div>
+ <div class="span12" style="background:gray">
+   Fluid 12
+   <div class="row-fluid">
+     <div class="span6" style="background:black">
+       Fluid 6
+       <div class="row-fluid">
+         <div class="span6" style="background:gray">Second Fluid</div>
+         <div class="span6" style="background:yellow">Second Fluid</div>
+        </div>
+      </div>
+      <div class="span6" style="background:red">Fluid Last</div>
+    </div>
+  </div>
 </div>
