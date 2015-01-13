461c7ae6305a0f749b0c880dc5c1c9387718ac02
cyloops <cyloops@gmail.com>
2015-01-13 17:21:54 +0900

    Bootstrap Fluid System 적용 Goldengrid 적용

--- application/views/head.php
+++ application/views/head.php
@@ -5,5 +5,7 @@
 		<!-- Bootstrap -->
 		<link href="/static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 		<link href="/static/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+		<link rel="stylesheet" href="/static/lib/goldengrid/GGS.css">
+		<script src="/static/lib/goldengrid/GGS.js"></script>
 	</head>
 	<body>
--- application/views/topic.php
+++ application/views/topic.php
@@ -1,12 +1,5 @@
 
-<div class="container-fluid">
-  <div class="row-fluid">
-    <div class="span2">
-	    <h1> Hello World! </h1>
-    </div>
-
-    <div class="span10">
-      Body content
-    </div>
-  </div>
+<div class="row-fluid">
+  <div class="span4">...</div>
+  <div class="span8">...</div>
 </div>
