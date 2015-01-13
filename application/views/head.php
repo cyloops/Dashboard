ce23bc260660723e04581be5d79632458d800702
cyloops <cyloops@gmail.com>
2015-01-13 10:35:10 +0900

    부트스트랩 반응형 설정

--- application/views/head.php
+++ application/views/head.php
@@ -1,1 +1,9 @@
-hello world!
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+		<link href="/static/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+		<link href="/static/lib/bootstrap/css/bootstrap-responsive.css" rel="stylesheet">
+	</head>
+	<body>
---
+++ application/views/topic.php
@@ -0,0 +1,12 @@
+
+<div class="container-fluid">
+  <div class="row-fluid">
+    <div class="span2">
+	    <h1> Hello World! </h1>
+    </div>
+
+    <div class="span10">
+      Body content
+    </div>
+  </div>
+</div>
