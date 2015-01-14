cfc53aa630e89b5db869eda103713f99c1115363
cyloops <cyloops@gmail.com>
2015-01-14 17:14:18 +0900

    bootstrap-responsive.css 수정하긴 전

--- application/views/topic.php
+++ application/views/topic.php
@@ -1,15 +1,28 @@
-<div class="row-fluid">
- <div class="span12" style="background:gray">
-   Fluid 12
-   <div class="row-fluid">
-     <div class="span6" style="background:black">
-       Fluid 6
-       <div class="row-fluid">
-         <div class="span6" style="background:gray">Second Fluid</div>
-         <div class="span6" style="background:yellow">Second Fluid</div>
-        </div>
+<div class="container-fluid">
+ <div class="row-fluid">
+   <div class="span2">Color set
+     <!--Color content-->
+      <div>
+        <div style="background:#292826;width:20px;height:20px;float:left"></div>
+        <div style="background:#3d332a;width:20px;height:20px;float:left"></div>
+        <div style="background:#605448;width:20px;height:20px;float:left"></div>
+        <div style="background:#ea7351;width:20px;height:20px;float:left"></div>
+        <div style="background:#f9c82e;width:20px;height:20px;float:left"></div>
       </div>
-      <div class="span6" style="background:red">Fluid Last</div>
-    </div>
-  </div>
+   </div>
+
+   <div class="span2">Color set
+      <div>
+        <div style="background:#292826;width:20px;height:20px;float:left"></div>
+        <div style="background:#3d332a;width:20px;height:20px;float:left"></div>
+        <div style="background:#605448;width:20px;height:20px;float:left"></div>
+        <div style="background:#ea7351;width:20px;height:20px;float:left"></div>
+        <div style="background:#f9c82e;width:20px;height:20px;float:left"></div>
+      </div>
+   </div>
+
+   <div class="span10">
+     <!--Body content-->
+   </div>
+ </div>
 </div>
