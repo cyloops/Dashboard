891fd576d9cc9cef681381a4ca5fb82269e7cbd9
cyloops <cyloops@gmail.com>
2015-01-16 18:38:19 +0900

    Color set test

--- application/views/topic.php
+++ application/views/topic.php
@@ -3,6 +3,16 @@
  <div class="row-fluid">
    <div class="span2"><span class="colorhead">flat color set</span>
      <!--Color content-->
+      <table>
+        <tr>
+          <td class="colorset" style="background:#292826"></td>
+          <td class="colorset" style="background:#3d332a"></td>
+          <td class="colorset" style="background:#605448"></td>
+          <td class="colorset" style="background:#ea7351"></td>
+          <td class="colorset" style="background:#f9c82e"></td>
+        </tr>
+      </table>
+
       <div>
         <div class="colorset" style="background:#292826"></div>
         <div class="colorset" style="background:#3d332a"></div>
